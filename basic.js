0fe7a36cd52ccf278479656a244e99d8d21fae0c
Akash A S <akashsumesh2004@gmail.com>
2023-12-30 12:03:36 +0530

    program completed

--- basic.js
+++ basic.js
@@ -1,67 +1,104 @@
 let choices=["rock","paper","scissor"];
+let win=0,lose=0,round,playerSelection,computerSelection;
 function getComputerChoice(){
     let random=Math.floor(Math.random()*choices.length);
     return choices[random];
 }
+function checkEmpty(){
+    if(classResult.childElementCount!=0){
+        classResult.removeChild(resultDis);
+    }
+}
 function playRound(playerSelection,computerSelection){
     if(playerSelection==="rock" && computerSelection==="paper"){
-        return "You Lose! Paper beats Rock";
+        return "You Lose! Rock was not able to move";
     }
     else if(playerSelection==="rock" && computerSelection==="scissor"){
-        return "You Win! Rock beats Scissor";
+        return "You Win! The scissor was destroyed";
     }
     else if(playerSelection==="paper" && computerSelection==="rock"){
-        return "You Win! Paper beats Rock";
+        return "You Win! Rock was not able tear your paper";
     }
     else if(playerSelection==="paper" && computerSelection==="scissor"){
-        return "You Lose! Scissor beats paper";
+        return "You Lose! You were torn into pieces";
     }
     else if(playerSelection==="scissor" && computerSelection==="rock"){
-        return "You Lose! Rock beats Scissor";
+        return "You Lose! The rock destroyed you";
     }
     else if(playerSelection==="scissor" && computerSelection==="paper"){
-        return "You Win! Scissor beats Paper";
+        return "You Win! You tore him to pieces";
     }
     else if(playerSelection===computerSelection){
-        return "tie";
-    }
-    else{
-        return "wrong";
+        return "Ohh you both chose the same";
     }
 }
-function game(){
-    let win=0;
-    let lose=0;
-    let round;
-    let playerSelection;
-    let computerSelection;
-    for(let i=0;i<5;i++){
-        playerSelection=prompt("Whats your choice");
-        playerSelection=playerSelection.toLowerCase();
-        computerSelection=getComputerChoice();
-        round=playRound(playerSelection,computerSelection);
-        console.log(round);
-        if(round==="tie"){
-            continue;
-        }
-        else if(round==="wrong"){
-            console.log("invalid choice");
-            console.log("Try again");
-            i--;
-            continue;
-        }
-        else if(round[4]==="W"){
+function result(roundResult){
+    if(win!=5 && lose!=5){
+        if(roundResult[4]=="W"){
+            checkEmpty();
+            resultDis.textContent=roundResult;
+            classResult.appendChild(resultDis);
             win++;
+            yourScore.textContent=win;
         }
-        else if(round[4]==="L"){
+        else if(roundResult[4]=="L"){
+            checkEmpty();
+            resultDis.textContent=roundResult;
+            classResult.appendChild(resultDis);
             lose++;
+            compScore.textContent=lose;
+        }
+        else if(roundResult=="tie"){
+            checkEmpty();
+            resultDis.textContent=roundResult;
+            classResult.appendChild(resultDis);
         }
     }
-    if(win>lose){
-        console.log("Ohhh! you have Won the Great war.")
+    if(win==5){
+        checkEmpty();
+        resultDis.textContent="Great champ you have destroyed COMP";
+        classResult.appendChild(resultDis);
+        classResult.appendChild(resetResult);
     }
-    else{
-        console.log("Sorry soldier you have been defeated.");
+    else if(lose==5){
+        checkEmpty();
+        resultDis.textContent="Sorry LOSER you were destroyed";
+        classResult.appendChild(resultDis);
+        classResult.appendChild(resetResult);
     }
 }
-game();
+const paper=document.querySelector(".paper");
+const rock=document.querySelector(".rock");
+const scissor=document.querySelector(".scissor");
+const classResult=document.querySelector(".result");
+const resultDis=document.createElement("h2");
+const resetResult=document.createElement('button');
+resetResult.textContent="REPLAY";
+resetResult.style.cssText="all:unset;width:120px;height:70px;background:red;text-align:center;font-size:20px;border-radius:8%"
+const yourScore=document.querySelector("#yourScore");
+const compScore=document.querySelector("#compScore");
+resetResult.onclick=()=>{
+    checkEmpty();
+    classResult.removeChild(resetResult);
+    yourScore.textContent="0";
+    compScore.textContent="0";
+    win=0;
+    lose=0;
+}
+
+paper.onclick=()=>{
+    playerSelection="paper";
+    round=playRound(playerSelection,getComputerChoice());
+    result(round);
+}
+rock.onclick=()=>{
+    playerSelection="rock";
+    round=playRound(playerSelection,getComputerChoice());
+    result(round);
+}
+scissor.onclick=()=>{
+    playerSelection="scissor";
+    round=playRound(playerSelection,getComputerChoice());
+    result(round);
+}
+    
